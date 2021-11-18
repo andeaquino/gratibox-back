eb67442b897ead14f029db87d1351ed485d47f24
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-11-18 18:19:23 -0300

    eslint and prettier installed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import "./src/setup.js";
-import app from "./src/app.js";
+import './src/setup.js';
+import app from './src/app.js';
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Running in mode ${process.env.NODE_ENV}`);
--- src/database.js
+++ src/database.js
@@ -1,14 +1,22 @@
-import pg from "pg";
+import pg from 'pg';
 
 const { Pool } = pg;
 
-const databaseConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-};
+const databaseConfig =
+  process.env.NODE_ENV === 'test'
+    ? {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+      }
+    : {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      };
 
 const connection = new Pool(databaseConfig);
 
